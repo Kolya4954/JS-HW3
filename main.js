3170bb5b4576e0f814034bbfafcf0fdbcdd1f43e
Коля <135145397+Kolya4954@users.noreply.github.com>
2025-10-01 16:00:55 +0300

    yay

---
+++ main.js
@@ -0,0 +1,46 @@
+const result = 5 + 5 + "5";
+
+console.log(result);
+
+console.log(typeof result);
+
+
+
+
+
+
+const email = "mykolas@mail.com";
+
+console.log(email.includes("@"));
+
+console.log(email.length);
+
+
+
+
+
+
+
+const one = "My";
+
+const two = "name";
+
+const three = "is";
+
+const fullName = one + " " + two + " " + three + " " + "Viktor";
+
+console.log(fullName);
+
+
+
+
+
+
+const userName = "Олександр";
+
+const payment = "300 гривень";
+
+const message = alert(`Дякуємо, ${userName}! До сплати ${payment}`);
+
+console.log(message);
+
